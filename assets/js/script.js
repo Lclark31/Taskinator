4fb0435c8fcd9876b4711a734b766a7ba458be4b
Lashaun <clarklas831@gmail.com>
2021-06-14 15:52:24 -0500

    user input submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-let formEl = document.querySelector("#task-form");
-let tasksToDoEl = document.querySelector("#tasks-to-do");
+let formEl = document.querySelector(`#task-form`);
+let tasksToDoEl = document.querySelector(`#tasks-to-do`);
 
 let createTaskHandler = function(event) {
     event.preventDefault();
-
-    let listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // set the value of the variables to the value of the input in the browser
+    let taskNameInput = document.querySelector(`input[name='task-name']`).value;
+    let taskTypeInput = document.querySelector(`select[name="task-type"]`).value;
+    
+    // create a li element in the variable and give it a class
+    let listItemEl = document.createElement(`li`);
+    listItemEl.className = `task-item`;
+    // create a div in the element with the id "tasks-to-do" and give it a class
+    let taskInfoEl = document.createElement(`div`);
+    taskInfoEl.className = `task-info`;
+    // add html to div variable
+    taskInfoEl.innerHTML = `<h3 class="task-name">` + taskNameInput + `</h3><span class="task-type">` + taskTypeInput + `</span>`;
+    // assign the variables as children elements to the pre-existing elements in the html
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener(`submit`, createTaskHandler);
